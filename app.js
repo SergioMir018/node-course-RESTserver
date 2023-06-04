c34982cbb8f42cae395e35ff5e4d5488ceda8805
Sergio Mir <voidtracer6@gmail.com>
2023-06-04 13:15:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+
+const port = process.env.PORT;
+const app = express();
+
+app.listen(port, () => {
+    console.log(`The app is listening at http://localhost:${port}`);
+});
