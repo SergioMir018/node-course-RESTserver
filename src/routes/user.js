6057b54b541e840047e9d4b113a8a51f7968f076
Sergio Mir <voidtracer6@gmail.com>
2023-06-08 16:55:11 -0400

    ./scr/controllers/users.js: validations added for required fields

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,7 +15,16 @@ router.patch('/', usersPatch)
 router.put('/:id',usersPut);
 
 router.post('/', [
+    check('name', 'The name is necessary').not().isEmpty(),
+    check('password', 'The password should be more than 6 characters').isLength({min:6}),
     check('email', 'The email is invalid').isEmail(),
+    check('role').custom(value => {
+        const validRoles = ['ADMIN_ROLE', 'USER_ROLE'];
+        if (!validRoles.includes(value.toUpperCase())) {
+            throw new Error('Not a valid role');
+        }
+        return true;
+    }),
 ], usersPost);
 
 router.delete('/', usersDelete);
